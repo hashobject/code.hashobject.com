0c319174b9b733cea90af72cce6df29614d46026
Anton Podviaznikov <podviaznikov@gmail.com>
2014-01-26 09:30:17 +0200

    update version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.4"]
+                 [hiccup "1.0.5"]
                  [pygdown "0.1.5"]
                  [sitemap "0.2.1"]
                  [leiningen-core "2.3.4"]]
