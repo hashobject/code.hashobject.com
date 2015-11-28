9fac4665b982650b47976c48de11de1d1002d317
Anton Podviaznikov <podviaznikov@gmail.com>
2015-11-27 23:59:23 -0800

    minors

--- src/code/hashobject/views/common.clj
+++ src/code/hashobject/views/common.clj
@@ -18,9 +18,4 @@
     [:p.license.small-12.columns "Except as otherwise noted, the content of this site is licensed
      under the <a href='http://creativecommons.org/licenses/by/3.0/'>Creative Commons Attribution 3.0 License</a>,
      and code samples are licensed under the
-     <a href='http://opensource.org/licenses/eclipse-1.0'>Eclipse Public License 1.0</a>."]
-    [:script {:src "//my.hellobar.com/e7784fac20054cc1170db3e6417786f1a864a9aa.js"
-              :type "text/javascript"
-              :charset "utf-8" :async "async"}]
-
-])
+     <a href='http://opensource.org/licenses/eclipse-1.0'>Eclipse Public License 1.0</a>."]])
